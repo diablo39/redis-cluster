21d7ab731a23fb09e67bc0c5838f9f203e9da946
Roman Glogowski <18666546+diablo39@users.noreply.github.com>
2019-12-08 21:18:26 +0100

    redis

--- redis.sh
+++ redis.sh
@@ -8,13 +8,30 @@ docker run -d --rm --name redis-3 --expose=7003 --expose=17003 --net=host -v $PW
 
 
 #create slaves
+docker run -d --rm --name redis-1-slave --expose=7011 --expose=17011 --net=host -v $PWD/redis-1-slave:/usr/local/etc/redis redis:5.0.7 redis-server /usr/local/etc/redis/redis.conf
+docker run -d --rm --name redis-2-slave --expose=7012 --expose=17012 --net=host -v $PWD/redis-2-slave:/usr/local/etc/redis redis:5.0.7 redis-server /usr/local/etc/redis/redis.conf
+docker run -d --rm --name redis-3-slave --expose=7013 --expose=17013 --net=host -v $PWD/redis-3-slave:/usr/local/etc/redis redis:5.0.7 redis-server /usr/local/etc/redis/redis.conf
 
-docker run --rm --name redis-1 -p 7011:7011 -p 17011:17011 -v C:/Sources/redis-cluster/docker-1:/usr/local/etc/redis docker-artifactrepo.mille.pl/redis:5.0.7 redis-server /usr/local/etc/redis/redis.conf
-docker run --rm --name redis-2 -p 7012:7012 -p 17012:17012 -v C:/Sources/redis-cluster/docker-2:/usr/local/etc/redis docker-artifactrepo.mille.pl/redis:5.0.7 redis-server /usr/local/etc/redis/redis.conf
-docker run --rm --name redis-3 -p 7013:7013 -p 17013:17013 -v C:/Sources/redis-cluster/docker-3:/usr/local/etc/redis docker-artifactrepo.mille.pl/redis:5.0.7 redis-server /usr/local/etc/redis/redis.conf
+# create tie-breaker
+docker run -d --rm --name redis-tie-breaker --expose=7000 --expose=17000 --net=host -v $PWD/redis-tie-breaker:/usr/local/etc/redis redis:5.0.7 redis-server /usr/local/etc/redis/redis.conf
 
 #redis-cli
 docker run --rm -it --net=host redis:5.0.7 bash
 
-# create cluster
+# create cluster with masters
 redis-cli --cluster create 172.18.35.139:7001 172.18.35.139:7002 172.18.35.139:7003 --cluster-replicas 0
+
+# add slaves
+redis-cli --cluster add-node 172.18.35.139:7011 172.18.35.139:7001 --cluster-slave
+redis-cli --cluster add-node 172.18.35.139:7012 172.18.35.139:7001 --cluster-slave
+redis-cli --cluster add-node 172.18.35.139:7013 172.18.35.139:7001 --cluster-slave
+
+# add tie braker
+redis-cli --cluster add-node 172.18.35.139:7000 172.18.35.139:7001
+
+docker kill redis-tie-breaker
+docker kill redis-1
+docker kill redis-2
+docker kill redis-3
+
+docker kill redis-tie-breaker && docker kill redis-1-slave && docker kill redis-2-slave && docker kill redis-3-slave
